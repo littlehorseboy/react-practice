9e1c39161658976587a950dd0a6e2f19d47889aa
littlehorseboy <littlehorseboy@gmail.com>
2018-11-18 11:04:16 +0800

    feat: 環境配置 v0.1
    todo:
    
    issue 環境配置

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: ['./src/index.js', './src/app.jsx'],
+  entry: ['./src/app.jsx'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './dist/'),
