f6cd956ac8faf770d1541514cdd67e1326722914
littlehorseboy <littlehorseboy@gmail.com>
2018-11-20 23:35:39 +0800

    feat: temperature
    todo:
    
    issue temperature

--- src/app.jsx
+++ src/app.jsx
@@ -1,34 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-class EasyForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.submitForm = this.submitForm.bind(this);
-    this.filebox = React.createRef();
-  }
-
-  submitForm(e) {
-    e.preventDefault();
-    if (this.filebox.current.files.length > 0) {
-      console.log(`選擇的檔案為: ${this.filebox.current.files[0].name}`);
-    } else {
-      console.log('沒有選擇任何檔案!');
-    }
-  }
-
+class Title extends React.Component {
   render() {
     return (
-      <form onSubmit={this.submitForm}>
-        <div>
-          <label htmlFor="filebox">上傳檔案:</label>
-          <input type="file" ref={this.filebox} />
-        </div>
-        <input type="submit" value="送出" />
-      </form>
+      <h1>{this.props.temperature >= 100 ? '達到沸點!!!' : '未達到沸點...'}</h1>
     );
   }
 }
 
-ReactDOM.render(<EasyForm />, document.querySelector('#app'));
+Title.propTypes = {
+  temperature: PropTypes.number,
+};
+
+class Celsius extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      temperature: 0,
+    };
+    this.changeState = this.changeState.bind(this);
+  }
+
+  changeState(e) {
+    const temperature = e.target.value;
+    this.setState({
+      [e.target.name]: temperature,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Title temperature={this.state.temperature} />
+        <div>目前攝氏溫度為: {this.state.temperature}</div>
+        <input type="number" name="temperature" value={this.state.temperature} onChange={this.changeState} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Celsius />, document.querySelector('#app'));
