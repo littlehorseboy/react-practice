2a1f5be6317fab48fc5aac36620a7cabc69ea66e
horse <littlehorseboy@gmail.com>
2018-12-04 17:45:30 +0800

    feat: for

---
+++ src/for/forSample.4.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import PropTypes from 'prop-types';
+
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map(number => <li key={number}>{number}</li>);
+
+ReactDOM.render(
+  <ul>
+    {listItems}
+  </ul>,
+  document.querySelector('#app'),
+);
