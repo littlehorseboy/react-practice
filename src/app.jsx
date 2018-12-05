04181e6789f04d2f0ac9a38c8fa0773c0dcafa3e
littlehorseboy <littlehorseboy@gmail.com>
2018-12-05 22:36:06 +0800

    feat: state up

--- src/app.jsx
+++ src/app.jsx
@@ -1,51 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-class Reservation extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isGoing: true,
-      numberOfGuests: 2,
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>水會燒開</p>;
   }
+  return <p>水不會燒開</p>;
+}
 
-  handleInputChange(e) {
-    const { target } = e;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const { name } = target;
-
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    console.log(this.state);
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>Is goint: </label>
-        <input name="isGoing" type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange} />
-
-        <br />
-
-        <label>Number of Guests: </label>
-        <input name="numberOfGuests" type="number" value={this.state.numberOfGuests} onChange={this.handleInputChange} />
-
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
+BoilingVerdict.propTypes = {
+  celsius: PropTypes.number,
 }
 
 ReactDOM.render(
-  <Reservation />,
+  <BoilingVerdict />,
   document.querySelector('#app'),
 );
